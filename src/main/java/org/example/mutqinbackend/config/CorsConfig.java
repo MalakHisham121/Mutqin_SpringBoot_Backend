0d9137d48784f2a4fa70263a78fe1233ff045157
MalakHisham121 <malak.hesham10011@gmail.com>
2025-10-03 13:06:01 +0300

    solve some cors error

---
+++ src/main/java/org/example/mutqinbackend/config/CorsConfig.java
@@ -0,0 +1,17 @@
+package org.example.mutqinbackend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://mutqin-team1.netlify.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
